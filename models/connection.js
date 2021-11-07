11b1d983c700c8cc9380ae1357a2722851f96c00
Gabriel Ferreira <glucas709@gmail.com>
2021-11-07 17:32:19 -0300

    feat: starting project and mongodb connection

---
+++ models/connection.js
@@ -0,0 +1,23 @@
+const { MongoClient } = require('mongodb');
+
+const DB_NAME = process.env.DB_NAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
+const uri = `mongodb+srv://gabriel-ferreira:${DB_PASSWORD}@cluster0.qiig4.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+
+const mongoClient = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const connection = async () => {
+  try {
+    await mongoClient.connect();
+    console.log('Connected correctly to server');
+    return client.db(DB_NAME);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+module.exports = connection;
